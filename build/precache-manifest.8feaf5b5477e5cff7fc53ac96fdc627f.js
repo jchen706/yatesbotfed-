946f55f4bd03e4cd190e2bd4cf1748d43739d580
Ren Silva <resilv@microsoft.com>
2019-05-31 09:44:41 +1000

    adding build

---
+++ build/precache-manifest.8feaf5b5477e5cff7fc53ac96fdc627f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40951a5ef8a96e7843c8176da7c7b8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba2a699fbd06a9f1a625",
+    "url": "/static/css/main.fbaeac03.chunk.css"
+  },
+  {
+    "revision": "e80bb9c7e1ee76425e21",
+    "url": "/static/js/2.dd9a2c21.chunk.js"
+  },
+  {
+    "revision": "ba2a699fbd06a9f1a625",
+    "url": "/static/js/main.477deaa1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
